cc357c20da9abebad3cb467741286e858537101d
thiago costa de jesus <thiagocostadejesus2@gmail.com>
2023-02-10 23:06:34 -0300

    "2023-02-10-UPDATE"

---
+++ Kotlin-4-devs-java/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Kotlin-4-devs-java"
+
---
+++ Kotlin-4-devs-java/src/main/kotlin/br/com/thiago/section04/WhenAndFor.kt
@@ -0,0 +1,24 @@
+package br.com.thiago.section04
+
+fun main(){
+
+    var x = 0
+    while (x < 10){
+        println("x = $x")
+
+        x += 3
+    }
+
+    do {
+        println("x = $x")
+        x -= 3
+    } while (x > 0)
+
+    for (item in 1 .. 10){
+        print("$item,")
+    }
+
+    for (ch in "Thiago" ){
+        print(ch)
+    }
+}
