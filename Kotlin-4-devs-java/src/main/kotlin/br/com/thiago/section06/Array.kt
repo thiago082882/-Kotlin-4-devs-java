33b44d76e2bbea321f86aac127ec26a0f58fb0fc
thiago costa de jesus <thiagocostadejesus2@gmail.com>
2023-02-10 23:06:34 -0300

    "2023-02-10-UPDATE"

---
+++ Kotlin-4-devs-java/src/main/kotlin/br/com/thiago/section06/Array.kt
@@ -0,0 +1,11 @@
+package br.com.thiago.section06
+
+import java.util.*
+
+fun main (args: Array<String>){
+
+var listaCompras = arrayOf("Laranja","Banana","Maça","Uva","Feijão","carne","Leite")
+
+//    println(Arrays.toString(listaCompras))
+    println(listaCompras.size)
+}
